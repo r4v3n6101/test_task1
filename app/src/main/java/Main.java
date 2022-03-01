2c4798cbbdda0ca7cfcf8e9fd077e7421594dec3
r4v3n6101 <raven6107@gmail.com>
2022-03-01 04:22:30 +0300

    Authorization

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,5 +1,15 @@
+import feign.Feign;
+import request.SumSubApi;
+import request.TokenAuthInterceptor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String appToken = System.getenv("APP_TOKEN");
+        String secret = System.getenv("SECRET");
+
+        SumSubApi api = Feign.builder()
+                .requestInterceptor(new TokenAuthInterceptor(appToken, secret))
+                .target(SumSubApi.class, "https://test-api.sumsub.com");
+        System.out.println(api.health());
     }
 }
---
+++ app/src/main/java/request/TokenAuthInterceptor.java
@@ -0,0 +1,48 @@
+package request;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.apache.commons.codec.binary.Hex;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+
+public class TokenAuthInterceptor implements RequestInterceptor {
+
+    private static final String TOKEN_HEADER = "X-App-Token";
+    private static final String SIGNATURE_HEADER = "X-App-Access-Sig";
+    private static final String TIMESTAMP_HEADER = "X-App-Access-Ts";
+
+    private final String appToken;
+    private final String secret;
+
+    public TokenAuthInterceptor(String appToken, String secret) {
+        this.appToken = appToken;
+        this.secret = secret;
+    }
+
+    @Override
+    public void apply(RequestTemplate template) {
+        long timestamp = Instant.now().getEpochSecond();
+        try {
+            template.header(TOKEN_HEADER, appToken);
+            template.header(SIGNATURE_HEADER, hmacSign(timestamp, template.method(), template.path(), template.body()));
+            template.header(TIMESTAMP_HEADER, String.valueOf(timestamp));
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String hmacSign(long timestamp, String method, String path, byte[] body) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac hmac = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        hmac.init(secretKey);
+        hmac.update((timestamp + method + path).getBytes(StandardCharsets.UTF_8));
+        byte[] result = body != null ? hmac.doFinal(body) : hmac.doFinal();
+        return Hex.encodeHexString(result);
+    }
+}
